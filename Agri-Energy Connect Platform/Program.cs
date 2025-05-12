a378b9b2a8959baa93bd25dffb4aaf93e82da22d
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-05-12 14:30:14 +0200

    Update identity and database initialization in Program.cs
    - Added using directive for `Agri_Energy_Connect_Platform.Models`.
    - Modified connection string retrieval to allow null values.
    - Updated identity service to use custom `ApplicationUser` and added role support.
    - Included `AddControllersWithViews()` for MVC setup.
    - Implemented database seeding with `DbInitializer.Seed(services)`.
    - Enhanced middleware pipeline with authentication and authorization.

--- Agri-Energy Connect Platform/Program.cs
+++ Agri-Energy Connect Platform/Program.cs
@@ -1,20 +1,33 @@
 using Agri_Energy_Connect_Platform.Data;
+using Agri_Energy_Connect_Platform.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+   
+    })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+    
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await DbInitializer.Seed(services);
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,6 +46,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
