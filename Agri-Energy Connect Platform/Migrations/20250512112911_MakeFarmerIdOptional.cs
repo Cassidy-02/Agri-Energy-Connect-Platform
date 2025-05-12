9ffe9042376e8609d476e63e733662b95815753e
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-05-12 14:32:07 +0200

    -updated fixes to database

---
+++ Agri-Energy Connect Platform/Migrations/20250512112911_MakeFarmerIdOptional.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agri_Energy_Connect_Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeFarmerIdOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Farmers_FarmerId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Farmers_FarmerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Farmers_FarmerId",
+                table: "AspNetUsers",
+                column: "FarmerId",
+                principalTable: "Farmers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Farmers_FarmerId",
+                table: "Products",
+                column: "FarmerId",
+                principalTable: "Farmers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Farmers_FarmerId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Farmers_FarmerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Farmers_FarmerId",
+                table: "AspNetUsers",
+                column: "FarmerId",
+                principalTable: "Farmers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Farmers_FarmerId",
+                table: "Products",
+                column: "FarmerId",
+                principalTable: "Farmers",
+                principalColumn: "Id");
+        }
+    }
+}
